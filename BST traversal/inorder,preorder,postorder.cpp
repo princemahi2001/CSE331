ea9720578dc7aacada55fac310106428eacf4afe
princemahi2001 <94538166+princemahi2001@users.noreply.github.com>
2023-08-28 14:02:07 +0530

    Update and rename inorder.cpp to inorder,preorder,postorder.cpp

--- BST traversal/inorder,preorder,postorder.cpp
+++ BST traversal/inorder,preorder,postorder.cpp
@@ -19,6 +19,22 @@ void inorder(struct Node* node)
     inorder(node->right);
 }
 
+void preorder(struct Node* node)
+{
+    if(node==NULL) return;
+    cout<<node->data<<" ";
+    preorder(node->left);
+    preorder(node->right);
+}
+
+void postorder(struct Node* node)
+{
+    if(node==NULL) return;
+    preorder(node->left);
+    preorder(node->right);
+    cout<<node->data<<" ";
+}
+
 int main()
 {
     struct Node* root=new Node(1);
@@ -26,8 +42,15 @@ int main()
     root->right=new Node(3);
     root->left->left=new Node(4);
     root->left->right=new Node(5);
-    //root->right->left=new Node(6);
-    
+    root->right->left=new Node(6);
+    root->right->right=new Node(7);
+    cout<<"inorder: ";
     inorder(root);
+    
+    cout<<endl<<"preorder: ";
+    preorder(root);
+    
+    cout<<endl<<"postorder: ";
+    postorder(root);
 }
 
